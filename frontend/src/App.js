a1f16d3f8989b828d8b11fdcf89724327594ce54
Caelin <caelouwho@gmail.com>
2021-07-28 21:07:05 -0400

    create app layout and routing basics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
-
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Error404 from "./pages/404";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <div className = "App" >
+            <Router>
+                <Navbar/>
+                <Switch>
+                    <Route exact path="/">
+                        <Home/>
+                    </Route>
+                    <Route path="*">
+                        <Error404/>
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
